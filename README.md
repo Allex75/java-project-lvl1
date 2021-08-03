6574647752f52181eda107d5c812efcaecfb8030
Алексей Лапушкин <allex_75@mail.ru>
2021-08-03 08:13:18 +0300

    readme2 commit

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/codeclimate/codeclimate/test_coverage)
 [![Actions Status](https://github.com/Allex75/java-project-lvl1/workflows/GithubActions/badge.svg)](https://github.com/Allex75/java-project-lvl1/actions)
+![example workflow](https://github.com/Allex75/java-project-lvl1/actions/workflows/main.yml/badge.svg)
+[![Build Status](https://travis-ci.com/Allex75/java-project-lvl1.svg?branch=main)](https://travis-ci.com/Allex75/java-project-lvl1)
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,13 +4,28 @@
 package hexlet.code;
 
 
+import java.util.Scanner;
+
 public class App {
 
 
-    public static void main(String[] args) {
+    public static void greeting(String[] args) {
 
         System.out.println("Welcome to the Brain Games!");
         Cli.name();
+
+        private static void main (String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Please enter the game number and press Enter ");
+            System.out.println("1 - Greet");
+            System.out.println("0 - Exit");
+            System.out.print("Your choice: ");
+            int number = sc.nextInt();
+            if (number == 1) {
+                return main();
+            }
+        }
+
 
     }
 }
