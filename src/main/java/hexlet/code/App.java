d28aa2cd479e38c9f0830309faa903988c82fcf8
Алексей Лапушкин <allex_75@mail.ru>
2021-08-03 08:37:52 +0300

    App2 commit

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,7 +4,7 @@
 package hexlet.code;
 
 
-import java.util.Scanner;
+
 
 public class App {
 
@@ -14,18 +14,18 @@ public class App {
         System.out.println("Welcome to the Brain Games!");
         Cli.name();
 
-        private static void main (String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Please enter the game number and press Enter ");
-            System.out.println("1 - Greet");
-            System.out.println("0 - Exit");
-            System.out.print("Your choice: ");
-            int number = sc.nextInt();
-            if (number == 1) {
-                return main();
-            }
+//        static void main (String[] args) {
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Please enter the game number and press Enter ");
+//            System.out.println("1 - Greet");
+//            System.out.println("0 - Exit");
+//            System.out.print("Your choice: ");
+//            int number = sc.nextInt();
+//            if (number == 1) {
+//                return greeting();
+//            }
         }
 
 
-    }
+
 }
