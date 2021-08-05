9af714ab4205f0410bee609eed6ed9a3c544eded
Алексей Лапушкин <allex_75@mail.ru>
2021-08-05 10:34:20 +0300

    Add Random function commit

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class Cli {
 
-    public static void name() {
+    public static String getName() {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("May I have your name? ");
-        String str = sc.nextLine();
-        System.out.println("Hello, " + str);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name);
+        return name;
     }
 }
